a1fa07371d62f6d9179b20a4060137b16d9f6ec9
George Boots <ggboots.99@gmail.com>
2022-01-25 15:44:38 +1100

    checks if page get reloaded

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 let arrow = document.getElementById("arrow");
 let arrowSwitch = true;
 
+// Copied from Stackoverflow
+// Learn PerformanceNavigationTiming
+//.map, .includes, /getEntriesByType
+const pageAccessedByReload = (
+    (window.PerformanceNavigationTiming && window.PerformanceNavigationTiming.type === 1) ||
+      window.performance
+        .getEntriesByType('navigation')
+        .map((nav) => nav.type)
+        .includes('reload')
+  );
+if(pageAccessedByReload === true){
+    arrowSwitch = true;
+    window.scrollBy(0,-870);
+}
+
 arrow.addEventListener("click", function(){
     if (arrowSwitch == true){
         window.scrollBy(0,870);
