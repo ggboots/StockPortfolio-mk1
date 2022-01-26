035b702ead3da304d19115f20052cdf80ab1106e
George Boots <ggboots.99@gmail.com>
2022-01-26 21:33:41 +1100

    @media queries for mobile + tablet

--- src/helper.js
+++ src/helper.js
@@ -1,9 +1,6 @@
 let arrow = document.getElementById("arrow");
 let arrowSwitch = true;
 
-// Copied from Stackoverflow
-// Learn PerformanceNavigationTiming
-//.map, .includes, /getEntriesByType
 const pageAccessedByReload = (
     (window.PerformanceNavigationTiming && window.PerformanceNavigationTiming.type === 1) ||
       window.performance
---
+++ src/index.js
@@ -0,0 +1,6 @@
+// import Helper from '/src/helper.js';
+
+// export {
+//     Helper,
+// }
+//best way of indexing multiple javascript files to html
